b1e27ea24c3751a3df09a5a3eabf6be33cf796c8
Muneeb Usmani <muneebusmani8355@gmail.com>
2023-10-30 19:09:01 +0500

    Lol

---
+++ 3/c#/Course Method/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Course_Method.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
